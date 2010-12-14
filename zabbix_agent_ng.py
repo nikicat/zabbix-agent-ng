d2075dc53c6630751af671f6f006a7e6df8b3a46
Nikolay Bryskin <nbryskin@yandex-team.ru>
2010-12-14 21:53:14 +0300

    new: migrated to 1.8 protocol, added item grouping for synchronous checking

--- zabbix_agent_ng.py
+++ zabbix_agent_ng.py
@@ -22,6 +22,8 @@ import config
 import daemon
 import daemon.pidlockfile
 import ldap
+import json
+import itertools
 from setproctitle import setproctitle
 
 class trapper(object):
@@ -30,42 +32,47 @@ class trapper(object):
         self.server = server
         self.port = port
         self.logger = logging.getLogger(host)
+        self.decoder = json.JSONDecoder()
+        self.encoder = json.JSONEncoder()
+
+    def update_items(self, results):
+        clock = int(time.time())
+        inner_data = []
+        for key, value in results:
+            self.logger.debug('updating item {0}={1}'.format(key, value))
+            inner_data.append({'host': self.host, 'key': key, 'value': value, 'clock': clock})
+        data = {'request': 'agent data', 'clock': clock, 'data': inner_data}
+        response = self._send_req(data)
+        if response[u'response'] != u'success':
+            raise RuntimeError(response)
+
+    def get_active_checks(self):
+        items = []
+        response = self._send_req({'request': 'active checks', 'host': self.host})
+        if response[u'response'] != u'success':
+            raise RuntimeError(response)
+        return map(lambda i: (i[u'key'], float(i[u'delay'])), response.get(u'data', []))
+
+    def item_not_supported(self, key):
+        self.update_item((key, 'ZBX_NOTSUPPORTED'))
+
+    def _send_req(self, data):
+        header = 'ZBXD\x01'
+        request = self.encoder.encode(data)
+        data_len = struct.pack('<Q', len(request))
+        self.logger.debug('sending request: {0}'.format(request))
+        msg = '{header}{data_len}{data}'.format(header=header, data_len=data_len, data=request)
+        sock = self._do_request(msg)
+        response = sock.read()
+        response = self.decoder.decode(response)
+        self.logger.debug('received response: {0}'.format(response))
+        return response
 
     def _do_request(self, data):
         sock = socket.socket()
         sock.connect((self.server, self.port))
         sock.send(data)
         return sock.makefile()
-
-    def update_item(self, key, data):
-        self.logger.debug('updating item {0}={1}'.format(key, data))
-        key = base64.b64encode(key)
-        data = base64.b64encode(str(data))
-        host = base64.b64encode(self.host)
-        request = '<req><host>{host}</host><key>{key}</key><data>{data}</data></req>'.format(**locals())
-        reply = self._do_request(request).read()
-        if reply != 'OK':
-            raise RuntimeError(reply)
-
-    def item_not_supported(self, key):
-        self.update_item(key, 'ZBX_NOTSUPPORTED')
-
-    def _send_req(self, data):
-        data_len = struct.pack('<Q', len(data))
-        header = 'ZBXD'
-        version = '\1'
-        msg = '{header}{version}{data_len}{data}'.format(**locals())
-        return self._do_request(msg)
-
-    def get_active_checks(self):
-        items = []
-        for line in self._send_req('ZBX_GET_ACTIVE_CHECKS\n{0}\n'.format(self.host)).readlines():
-            if line[:-1] == 'ZBX_EOF':
-                break
-            key, refresh_time = line.split(':')[:2]
-            self.logger.debug('received active check {0}'.format(line[:-1]))
-            items.append((key, int(refresh_time)))
-        return items
 
 class script(object):
     bin_dir = '/etc/zabbix/bin'
@@ -77,39 +84,43 @@ class script(object):
         self.execute = self.execute_shell
         if self.command.split()[0].endswith('.py'):
             module = __import__(self.command.split()[0][:-3])
-            if hasattr(module, 'main'):
-                self.module_main = module.main
+            if hasattr(module, 'main') or hasattr(module, 'vmain'):
+                self.module = module
                 self.execute = self.execute_module
 
-    def execute_module(self, *args):
-        logging.debug('calling {0}.main({1})'.format(self.module_main.__module__, ', '.join(args)))
-        result = self.module_main(*args)
-        if type(result) is float:
-            result = '{0:f}'.format(result)
+    def execute_module(self, args_combinations):
+        if hasattr(self.module, 'vmain'):
+            logging.debug('calling {0}.vmain({1})'.format(self.module, args_combinations))
+            result = self.module.vmain(args_combinations)
+        else:
+            logging.debug('calling {0}.main({1})'.format(self.module, args_combinations))
+            result = [self.module.main(*args) for args in args_combinations]
         return result
 
-    def execute_shell(self, *args):
-        cmd = self.command
-        for i in range(10):
-            cmd = cmd.replace('${0}'.format(i+1), i < len(args) and args[i] or '')
-        logging.debug('invoking shell: {0}'.format(cmd))
-        proc = subprocess.Popen(cmd, cwd=self.bin_dir, stdout=subprocess.PIPE, shell=True)
-        output = proc.communicate()[0].split('\n', 1)[0]
-        if proc.returncode != 0:
-            raise subprocess.CalledProcessError(proc.returncode, cmd)
-        return output
+    def execute_shell(self, args_combinations):
+        result = []
+        for args in args_combinations:
+            cmd = self.command
+            for i in range(10):
+                cmd = cmd.replace('${0}'.format(i+1), i < len(args) and args[i] or '')
+            logging.debug('invoking shell: {0}'.format(cmd))
+            proc = subprocess.Popen(cmd, cwd=self.bin_dir, stdout=subprocess.PIPE, shell=True)
+            result.append(proc.communicate()[0].split('\n', 1)[0])
+            if proc.returncode != 0:
+                raise subprocess.CalledProcessError(proc.returncode, cmd)
+        return result
 
 sys.path.append(script.bin_dir)
 os.environ['PATH'] = os.pathsep.join([os.environ['PATH'], script.bin_dir])
 
 class item(object):
-    def __init__(self, host, key, interval, script, args, trapper):
+    def __init__(self, host, key, interval, script, args):
+        self.host = host
         self.key = key
         self.interval = interval
         self.script = script
         self.logger = logging.getLogger(host)
         self.args = [arg == '$hostname' and host or arg for arg in args]
-        self.trapper = trapper
 
     def __eq__(self, other):
         return self.key == other.key and self.interval == other.interval
@@ -120,7 +131,23 @@ class item(object):
     def __str__(self):
         return self.key
 
+class coupled_item(object):
+    def __init__(self, script, trapper):
+        self.items = set()
+        self.script = script
+        self.logger = logging.getLogger('coupled_item.{0}'.format(script))
+        self.trapper = trapper
+
+    def update(self, items):
+        assert len(items) > 0
+        for i in items:
+            assert i.script == self.script, 'trying to insert item with different script to coupled_item: {0} != {1}'.format(i.script, self.script)
+        self.items |= set(items)
+        self.interval = min(self.items, key=lambda i: i.interval).interval
+
     def check_loop(self):
+        assert len(self.items) > 0
+        assert type(self.interval) is float, 'type of interval is {0}'.format(type(self.interval))
         while True:
             try:
                 self.check()
@@ -129,10 +156,10 @@ class item(object):
             time.sleep(self.interval)
 
     def check(self):
-        self.logger.debug('executing script {0}[{1}]'.format(self.script.key, ', '.join(self.args)))
-        result = self.script.execute(*self.args)
-        self.logger.debug('result of script {0}[{1}] = {2}'.format(self.script.key, ', '.join(self.args), result))
-        self.trapper.update_item(self.key, result)
+        items = self.items
+        args_combinations = [i.args for i in items]
+        results = self.script.execute(args_combinations)
+        self.trapper.update_items([(i.key, r) for i, r in zip(items, results)])
 
 class host(object):
     def __init__(self, name, options, scripts):
@@ -142,6 +169,7 @@ class host(object):
         self.logger = logging.getLogger(name)
         self.trapper = trapper(name, options.server, options.port)
         self.items = set()
+        self.coupled_items = []
 
     def loop(self):
         while True:
@@ -153,24 +181,43 @@ class host(object):
     def update_active_checks(self):
         try:
             self.logger.debug('updating item list')
-            items = set()
+            retrieved_items = set()
             for raw_key, interval in self.trapper.get_active_checks():
                 key, bare_key, args = self.item_re.match(raw_key).group(1, 2, 4)
                 for script in self.scripts:
                     if script.key == bare_key:
-                       items.add(item(self.name, key, interval, script, args and args.split(',') or [], self.trapper))
+                       retrieved_items.add(item(self.name, key, interval, script, args and args.split(',') or []))
                        break
-            added_items = items - self.items
-            removed_items = self.items - items
+            added_items = retrieved_items - self.items
+            removed_items = self.items - retrieved_items
+            self.items -= removed_items
+            self.items |= added_items
             if added_items:
                 self.logger.info('added items: {0}'.format(', '.join(map(str, added_items))))
             if removed_items:
                 self.logger.info('removed items: {0}'.format(', '.join(map(str, removed_items))))
-            gevent.killall([i.job for i in removed_items], block=True)
-            self.items -= removed_items
-            for i in added_items:
-                i.job = gevent.spawn(i.check_loop)
-            self.items |= added_items
+            for ci in self.coupled_items:
+                ci.items -= removed_items
+            jobs_to_kill = []
+            for ci in self.coupled_items[:]:
+                if len(ci.items) == 0:
+                    jobs_to_kill.append(ci.job)
+                    self.coupled_items.remove(ci)
+            gevent.killall(jobs_to_kill, block=True)
+
+            added_items = sorted(list(added_items), key=lambda i: i.script)
+            grouped_items = itertools.groupby(added_items, lambda i: i.script)
+            for script, items in grouped_items:
+                cis = [ci for ci in self.coupled_items if ci.script == script]
+                if len(cis) > 0:
+                    assert len(cis) == 1, 'more than one coupled_item for script {0}'.format(script)
+                    ci.update(list(items))
+                else:
+                    ci = coupled_item(script, self.trapper)
+                    ci.update(list(items))
+                    ci.job = gevent.spawn(ci.check_loop)
+                    self.coupled_items.append(ci)
+
             if not self.items:
                 self.logger.info('no items')
         except Exception, e:
