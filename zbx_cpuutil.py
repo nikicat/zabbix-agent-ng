2a2332e31bfd629fa5be1d567a09b5cd42a013b3
Nikolay Bryskin <nbryskin@yandex-team.ru>
2011-03-25 16:23:28 +0300

    zbx_cpuutil optimization: vmain support

--- zbx_cpuutil.py
+++ zbx_cpuutil.py
@@ -1,15 +1,26 @@
-def main(cpu, counter_name):
+def get_stat(lines, cpu, counter_name):
     if cpu == 'all':
         cpu = 'cpu '
     else:
         cpu = 'cpu{0} '.format(cpu)
-    for line in open('/proc/stat').readlines():
+    for line in lines:
         if line.startswith(cpu):
             user, nice, system, idle, wait, irq, softirq, steal, guest = line[4:].split(' ')[1:10]
             if type(counter_name) is list:
                 return [locals()[name] for name in counter_name]
             else:
                 return locals()[counter_name]
+
+def main(cpu, counter_name):
+    return get_stat(open('/proc/stat').readlines(), cpu, counter_name)
+
+def vmain(combinations):
+    lines = open('/proc/stat').readlines()
+    results = []
+    for args in combinations:
+        results.append(get_stat(lines, *args))
+
+    return results
 
 if __name__ == '__main__':
     import sys
