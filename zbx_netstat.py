aed9d946decf8c29a3cda36b2e6dee1ad1db643c
Nikolay Bryskin <nbryskin@yandex-team.ru>
2011-04-02 23:24:57 +0400

    zbx_netstat: expressions support and previous value in locals()
    Previous statistics values can be accessed with 'prev.' prefix

--- zbx_netstat.py
+++ zbx_netstat.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python
 
 from itertools import izip, chain
+from collections import namedtuple
+import logging
 
 def get_stat():
     lines_snmp = open('/proc/net/snmp').readlines()
@@ -11,19 +13,35 @@ def get_stat():
         group1, namelist = names.split(':')
         group2, valuelist = values.split(':')
         assert(group1 == group2)
-        groups[group1] = dict(zip(namelist.split(), valuelist.split()))
+        group_type = namedtuple(group1, namelist.split())
+        groups[group1] = group_type(*map(int, valuelist.split()))
     return groups
 
-def main(type, name):
-    return get_stat()[str(type)][str(name)]
+def main(expr):
+    return vmain([expr])[0]
+
+prev_stat = None
 
 def vmain(combinations):
     stat = get_stat()
+    stat_tuple_type = namedtuple('Stat', stat.keys())
+    stat_tuple = stat_tuple_type(*stat.values())
+    global prev_stat
+    if prev_stat is None:
+        prev_stat = stat_tuple
     results = []
-    for type, name in combinations:
-        results.append(stat[str(type)][str(name)])
+    for expr in combinations:
+        try:
+            locals_ = stat
+            locals_.update({'prev': prev_stat})
+            result = eval(expr[0], {}, locals_)
+        except Exception, e:
+            logging.getLogger('zabbix-agent-ng').warning('failed to evaluate expression {0} with locals={1}: {2}'.format(expr, stat, e))
+            result = None
+        results.append(result)
+    prev_stat = stat_tuple
     return results
 
 if __name__ == '__main__':
     import sys
-    print(main(sys.argv[1], sys.argv[2]))
+    print(main(sys.argv[1]))
